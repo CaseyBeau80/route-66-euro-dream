63b529cfc35a37e015d0edd2ee975d60ed09012f
CaseyBeau80 <caseybeau@gmail.com>
2025-05-17 08:53:45 -0500

    Create Route66Map.tsx

---
+++ src/components/Route66Map.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Route66Map = () => {
+  return (
+    <div className="my-8 px-4">
+      <iframe
+        src="https://genuine-taffy-ad3e9e.netlify.app"
+        width="100%"
+        height="600"
+        style={{ border: "none", borderRadius: "12px" }}
+        title="Route 66 Interactive Map"
+      ></iframe>
+    </div>
+  );
+};
+
+export default Route66Map;
